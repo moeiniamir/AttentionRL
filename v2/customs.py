2907b393151896daff40054a0e95216f1fa25d9a
Amir Moeini <moeiniamir@outlook.com>
2023-07-12 09:16:00 +0000

    fix bugs. pushing for server

---
+++ v2/customs.py
@@ -0,0 +1,68 @@
+import logging
+
+from tianshou.data import Batch
+from tianshou.trainer.offpolicy import *
+import torch
+from transformers.models.vit.modeling_vit import *
+from tianshou.policy import DQNPolicy
+
+class CustomDQNPolicy(DQNPolicy):
+    def exploration_noise(
+            self,
+            act: Union[np.ndarray, Batch],
+            batch: Batch,
+    ) -> Union[np.ndarray, Batch]:
+        print(f"{act=}")
+        return super().exploration_noise(act, batch)
+
+
+class CustomOffpolicyTrainer(OffpolicyTrainer):
+    def policy_update_fn(self, data: Dict[str, Any], result: Dict[str, Any]) -> None:
+        """Perform off-policy updates."""
+        assert self.train_collector is not None
+        for _ in range(round(self.update_per_step * result["n/st"])):
+            self.gradient_step += 1
+            losses = self.policy.update(self.batch_size, self.train_collector.buffer)
+            self.log_update_data(data, losses)
+
+
+def custom_offpolicy_trainer(*args, **kwargs) -> Dict[str, Union[float, str]]:  # type: ignore
+    return CustomOffpolicyTrainer(*args, **kwargs).run()
+
+
+class CustomViTEmbeddings(ViTEmbeddings):
+    def forward(
+            self,
+            pixel_values: torch.Tensor,
+            bool_masked_pos: Optional[torch.BoolTensor] = None,
+            interpolate_pos_encoding: bool = False,
+    ) -> torch.Tensor:
+        batch_size, num_channels, height, width = pixel_values.shape
+        embeddings = self.patch_embeddings(pixel_values, interpolate_pos_encoding=interpolate_pos_encoding)
+
+        if bool_masked_pos is not None:
+            seq_length = embeddings.shape[1]
+            mask_tokens = self.mask_token.expand(batch_size, seq_length, -1)
+            # replace the masked visual tokens by mask_tokens
+            mask = bool_masked_pos.type_as(mask_tokens)
+            embeddings = embeddings * (1.0 - mask) + mask_tokens * mask
+
+        # add the [CLS] token to the embedded patch tokens
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        embeddings = torch.cat((cls_tokens, embeddings), dim=1)
+
+        # add positional encoding to each token
+        if interpolate_pos_encoding:
+            embeddings = embeddings + self.interpolate_pos_encoding(embeddings, height, width)
+        else:
+            embeddings = embeddings + self.position_embeddings
+
+        embeddings = self.dropout(embeddings)
+
+        return embeddings
+
+
+class CustomViTModel(ViTModel):
+    def __init__(self, config: ViTConfig, add_pooling_layer: bool = True, use_mask_token: bool = False):
+        super().__init__(config, add_pooling_layer, use_mask_token)
+        self.embeddings = CustomViTEmbeddings(config, use_mask_token=use_mask_token)
