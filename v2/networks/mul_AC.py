c8fc19314bf89ecfb99b92cfdf0f7511bccc0a1c
Amir Moeini <amirmoini@outlook.com>
2023-08-29 17:51:09 +0330

    add simmim, add beit visualization

--- v2/networks/mul_AC.py
+++ v2/networks/mul_AC.py
@@ -19,7 +19,7 @@ class Actor(nn.Module):
         self.t = np.sqrt(1024)
 
     def forward(self, obs, **kwargs):
-        curr, adj = self.basenet(obs, **kwargs)
+        curr, adj = self.basenet(obs)
         curr = self.curr_linear(curr)
         adj = self.adj_linear(adj)
         pi = einops.einsum(curr, adj, 'i k, i j k -> i j')
@@ -31,11 +31,12 @@ class Critic(nn.Module):
     def __init__(self, basenet):
         super().__init__()
         self.basenet = basenet
-        self.linear = nn.Linear(basenet.vit.config.hidden_size, 1)
-        self.cross_attention = nn.TransformerDecoder(nn.TransformerDecoderLayer(384, 4, 384, 0.1, batch_first=True), 2)
+        hidden_size = basenet.vit.config.hidden_size
+        self.linear = nn.Linear(hidden_size, 1)
+        self.cross_attention = nn.TransformerDecoder(nn.TransformerDecoderLayer(hidden_size, 4, hidden_size, 0.1, batch_first=True), 2)
 
     def forward(self, obs, **kwargs):
-        curr, adj = self.basenet(obs, **kwargs)
+        curr, adj = self.basenet(obs)
         ca_out = self.cross_attention(curr.unsqueeze(1), adj).squeeze()
         value = self.linear(ca_out)
         return value
--- v2/networks/path_vit.py
+++ v2/networks/path_vit.py
@@ -146,38 +146,3 @@ class BaseNetwork(nn.Module):
             return lhs[:, 0], None
         else:
             return lhs[:, 0], lhs[:, 1:5]
-
-
-class Actor(nn.Module):
-    def __init__(self, basenet):
-        super().__init__()
-        self.basenet = basenet
-        # self.curr_linear = nn.Linear(basenet.vit.config.hidden_size, basenet.vit.config.hidden_size)
-        self.curr_linear = nn.Linear(basenet.vit.config.hidden_size, 1024)
-        # self.adj_linear = nn.Linear(basenet.vit.config.hidden_size, basenet.vit.config.hidden_size)
-        self.adj_linear = nn.Linear(basenet.vit.config.hidden_size, 1024)
-        # self.t = nn.Parameter(torch.tensor(100, dtype=torch.float32))
-        self.t = np.sqrt(1024)
-
-    def forward(self, obs, **kwargs):
-        curr, adj = self.basenet(obs, **kwargs)
-        curr = self.curr_linear(curr)
-        adj = self.adj_linear(adj)
-        pi = einops.einsum(curr, adj, 'i k, i j k -> i j')
-        pi = pi / self.t
-        return pi, None
-
-
-class Critic(nn.Module):
-    def __init__(self, basenet):
-        super().__init__()
-        self.basenet = basenet
-        self.linear = nn.Linear(basenet.vit.config.hidden_size, 1)
-        self.cross_attention = nn.TransformerDecoder(
-            nn.TransformerDecoderLayer(384, 4, 384, 0.1, batch_first=True), 2)
-
-    def forward(self, obs, **kwargs):
-        curr, adj = self.basenet(obs, **kwargs)
-        ca_out = self.cross_attention(curr.unsqueeze(1), adj).squeeze()
-        value = self.linear(ca_out)
-        return value
