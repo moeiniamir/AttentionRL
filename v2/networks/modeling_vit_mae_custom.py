52a775ebe566994c2ec55927fc9f489b1aef30a8
Amir Moeini <amirmoini@outlook.com>
2023-10-24 17:17:02 +0330

    fix bug

--- v2/networks/modeling_vit_mae_custom.py
+++ v2/networks/modeling_vit_mae_custom.py
@@ -369,8 +369,8 @@ class ViTMAESelfAttention(nn.Module):
         # kmask.shape = (batch_size, seq_len)
         # attention_scores.shape = (batch_size, num_heads, seq_len, seq_len)
         kmask = nn.functional.pad(kmask, (1, 0), value=True)
-        attention_scores = torch.where(kmask.unsqueeze(-1).unsqueeze(1), -1e9, attention_scores)
-        attention_scores = torch.where(kmask.unsqueeze(1).unsqueeze(1), -1e9, attention_scores)
+        attention_scores = torch.where(kmask.unsqueeze(-1).unsqueeze(1), attention_scores, -1e9)
+        attention_scores = torch.where(kmask.unsqueeze(1).unsqueeze(1), attention_scores, -1e9)
         #!
 
         # Normalize the attention scores to probabilities.
