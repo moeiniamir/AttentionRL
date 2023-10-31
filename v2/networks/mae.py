525e76d09065b60906bdd4b6da5c8b9d226e1586
Amir Moeini <amirmoini@outlook.com>
2023-10-31 11:39:16 +0330

    add projectedOrdEmbMae

--- v2/networks/mae.py
+++ v2/networks/mae.py
@@ -141,7 +141,72 @@ class OrderEmbeddingBaseNetwork(BaseNetwork):
             self.store_output = False
 
         return output
-    
+
+
+class ProjOrdEmbBaseNetwork(BaseNetwork):
+    def __init__(self, patch_size, n_last_positions, ord_emb_dim=4, *args, **kwargs):
+        super().__init__(patch_size, 0, *args, **kwargs)
+        self.n_last_positions = n_last_positions
+        if self.n_last_positions > 0:
+            self.order_embeddings = nn.Parameter(
+                torch.zeros(1, self.n_last_positions, ord_emb_dim), requires_grad=True
+            )
+            self.order_embedding_projection = nn.Linear(ord_emb_dim, self.vit.config.hidden_size)
+        pos_emb = get_2d_sincos_pos_embed(self.vit.config.hidden_size, int(
+            self.vit.embeddings.patch_embeddings.num_patches**0.5), add_cls_token=False)
+        pos_emb = torch.from_numpy(pos_emb).unsqueeze(0).to(torch.float32)
+        self.register_buffer('mae_pos_emb', pos_emb)
+
+    def forward(self, obs, state=-1, **kwargs):
+        if self.stored_output is not None:
+            stored_output = self.stored_output
+            self.stored_output = None
+            return stored_output
+
+        history = obs['history']
+        kmask = history['kmask'][:, ::self.vit_patch_size,
+                                 ::self.vit_patch_size].flatten(1).to(self.vit.device)
+        canvas = history['history'].to(torch.float32).to(self.vit.device)
+        out = self.vit(
+            pixel_values=canvas,
+            last_positions=None,
+            padded_mask=None,
+            kmask=kmask,
+        )
+
+        last_positions = history['last_positions']
+        last_positions = self.build_indices(
+            last_positions[..., 0], last_positions[..., 1], canvas).to(self.vit.device)
+        
+        lhs = out['last_hidden_state'][:, 1:]
+        
+        # add ord embedding
+        idx = last_positions.unsqueeze(-1).repeat(1, 1, lhs.shape[-1])
+        padded_mask = history['padded_mask'].to(self.vit.device)
+        final_order_embeddings = self.order_embedding_projection(self.order_embeddings)
+        src = final_order_embeddings.repeat(
+            lhs.shape[0], 1, 1) * (~padded_mask.unsqueeze(-1))
+        lhs.scatter_add_(1, idx, src)
+        
+        if 'running_kmask' in history:
+            running_kmask = history['running_kmask'][:, ::self.vit_patch_size,
+                                                     ::self.vit_patch_size].flatten(1).to(self.vit.device)
+            urdl = history['urdl']
+            urdl = self.build_indices(
+                urdl[..., 0], urdl[..., 1], canvas).to(self.vit.device)
+        else:
+            running_kmask = None
+            urdl = None
+
+        # add pos embedding
+        lhs = lhs + self.mae_pos_emb
+
+        output = (lhs, kmask, last_positions, urdl, running_kmask)
+        if self.store_output:
+            self.stored_output = output
+            self.store_output = False
+
+        return output
     
 class PerStepMAE(OrderEmbeddingBaseNetwork):
     def __init__(self, patch_size, n_last_positions, *args, **kwargs):
