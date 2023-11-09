15d974d6774fbc0bbcd84d94f48dfcacf65d76da
Amir Moeini <amirmoini@outlook.com>
2023-11-09 21:54:10 +0330

    CustomSubprocVectorEnv: log metadata of env

--- v2/customs.py
+++ v2/customs.py
@@ -1,4 +1,6 @@
+from typing import Any, Callable, List
 from tianshou.data import Batch
+from tianshou.env.utils import ENV_TYPE
 from tianshou.trainer.offpolicy import *
 from transformers.models.vit.modeling_vit import *
 from tianshou.policy import DQNPolicy
@@ -29,14 +31,12 @@ class CustomDQNPolicy(DQNPolicy):
             act: Union[np.ndarray, Batch],
             batch: Batch,
     ) -> Union[np.ndarray, Batch]:
-        # print(f"{act=}", end='')
         return super().exploration_noise(act, batch)
 
 
 class CustomOffpolicyTrainer(OffpolicyTrainer):
     def policy_update_fn(self, data: Dict[str, Any], result: Dict[str, Any]) -> None:
         """Perform off-policy updates."""
-        # print(f"{data=}\n{result=}")
         assert self.train_collector is not None
         for _ in range(round(self.update_per_step * result["n/st"])):
             self.gradient_step += 1
@@ -44,6 +44,10 @@ class CustomOffpolicyTrainer(OffpolicyTrainer):
             self.log_update_data(data, losses)
 
 class CustomSubprocVectorEnv(ts.env.SubprocVectorEnv):
+    def __init__(self, *args, wandb_logger=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wandb_logger = wandb_logger
+    
     def mix_envs(self, action_list, episode_len):
         # build a list of numbers of len env_num whose numbers are evenly distributed in [0, episode_len)
         action_count_list = np.linspace(0, episode_len, self.env_num + 1, dtype=np.int32, endpoint=False)[1:]
@@ -54,4 +58,17 @@ class CustomSubprocVectorEnv(ts.env.SubprocVectorEnv):
                     assert not truncated, "The episode should not be truncated in mixing"
                     if terminated:
                         self.workers[i].send(None)
+                        
+    def step(
+        self,
+        *args,
+        **kwargs
+    ):
+        out = super().step(*args, **kwargs)
+        if self.wandb_logger and self.wandb_logger.use_wandb:
+            metas = out[4]
+            for meta in metas:
+                if meta['logs']:
+                    self.wandb_logger.wandb_run.log(meta['logs'])
+        return out
 
