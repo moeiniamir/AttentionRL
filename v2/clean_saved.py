371fec51c60c667b24873c4a6504385a83cc955d
Amir Moeini <amirmoini@outlook.com>
2024-02-27 09:42:08 +0330

    feat[clean_saved]: add delete_less_progressed_than

--- v2/clean_saved.py
+++ v2/clean_saved.py
@@ -7,9 +7,9 @@ app = typer.Typer()
 @app.command()
 def delete_older_than(days: int):
     for file in Path('saved/archive').glob('*'):
+        print(datetime.now() - datetime.fromtimestamp(file.stat().st_ctime))
         if file.is_file() and 'max' not in file.name and datetime.now() - datetime.fromtimestamp(file.stat().st_ctime) > timedelta(days=days):
             file.unlink()
-            
 
 @app.command()
 def archive_saved(max_too: bool = False):
@@ -17,5 +17,12 @@ def archive_saved(max_too: bool = False):
         if file.is_file() and (max_too or 'max' not in file.name):
             file.rename(Path('saved/archive') / file.name)
             
+@app.command()
+def delete_less_progressed_than(steps: int):
+    for file in Path('saved').glob('*'):
+        if file.is_file() and int(file.stem.split('_')[-1]) < steps:
+            # print(file)
+            file.unlink()
+
 if __name__ == '__main__':
     app()
